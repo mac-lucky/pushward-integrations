e388617b78e251ad5e6c2f36b42f098940d408f2
maclucky <44085390+mac-lucky@users.noreply.github.com>
2026-02-15 03:52:20 +0100

    fix: gitignore pattern was excluding cmd/pushward-github directory

---
+++ github/cmd/pushward-github/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/mac-lucky/pushward-docker/github/internal/config"
+	ghclient "github.com/mac-lucky/pushward-docker/github/internal/github"
+	"github.com/mac-lucky/pushward-docker/github/internal/poller"
+	"github.com/mac-lucky/pushward-docker/github/internal/pushward"
+)
+
+func main() {
+	configPath := flag.String("config", "config.yml", "path to config file")
+	flag.Parse()
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	slog.SetDefault(logger)
+
+	cfg, err := config.Load(*configPath)
+	if err != nil {
+		slog.Error("failed to load config", "error", err)
+		os.Exit(1)
+	}
+
+	gh := ghclient.NewClient(cfg.GitHub.Token)
+	pw := pushward.NewClient(cfg.PushWard.URL, cfg.PushWard.APIKey)
+
+	p := poller.New(cfg, gh, pw)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	slog.Info("starting pushward-github", "repos", cfg.GitHub.Repos, "slug", cfg.PushWard.ActivitySlug)
+	if err := p.Run(ctx); err != nil && ctx.Err() == nil {
+		slog.Error("poller exited with error", "error", err)
+		os.Exit(1)
+	}
+	slog.Info("shutdown complete")
+}
